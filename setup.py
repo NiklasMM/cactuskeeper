43c6a9dd31be23481e1394c54bb5da668e9f25cb
Niklas Meinzer <github@niklas-meinzer.de>
2017-04-15 18:59:48 +0200

    feat(travis): Add .travis.yml

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='cactuskeeper',
     version='0.1',
-    py_modules='cactuskeeper',
+    packages=find_packages(),
     install_requires=[
         'Click',
         'gitpython'
