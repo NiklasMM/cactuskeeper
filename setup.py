7a9b3b5c6fb22be3070abf1403723c1928c9275f
Niklas Meinzer <mail@niklas-meinzer.de>
2017-04-16 11:37:21 +0200

    refac(setup): Move requirements to files

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as requirements:
+    install_requires = requirements.read().splitlines()
+
+with open('test_requirements.txt') as requirements:
+    test_requires = requirements.read().splitlines()
+
 setup(
     name='cactuskeeper',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'Click',
-        'gitpython'
-    ],
+    install_requires=install_requires,
+    tests_require=test_requires,
     entry_points='''
         [console_scripts]
         ck=cactuskeeper.cli:cli
