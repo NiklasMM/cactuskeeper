38b0829aa6779ed63f46786af57beabf6d946b97
Niklas Meinzer <github@niklas-meinzer.de>
2019-05-30 21:41:21 +0200

    dev(dept): Use pip-tools to pin dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as requirements:
     install_requires = requirements.read().splitlines()
 
-with open("test_requirements.txt") as requirements:
-    test_requires = requirements.read().splitlines()
+with open("dev_requirements.txt") as requirements:
+    dev_requirements = requirements.read().splitlines()
 
 setup(
     name="cactuskeeper",
     version="0.1",
     packages=find_packages(),
     install_requires=install_requires,
-    tests_require=test_requires,
+    extras_require={"dev": dev_requirements},
     entry_points="""
         [console_scripts]
         ck=cactuskeeper.cli:cli
